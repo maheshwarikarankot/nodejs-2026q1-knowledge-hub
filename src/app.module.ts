d55c73be54b18146e69b320bff365697e4493d89
maheshwarikarankot <maheshwari635@gmail.com>
2026-04-04 20:44:17 -0500

    updated app.module.ts & added logging interceptor file-measure duration and access the response after the handler completes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { UserModule } from './user/user.module';
 import { ArticleModule } from './article/article.module';
 import { CategoryModule } from './category/category.module';
 import { CommentModule } from './comment/comment.module';
+import { LoggingInterceptor } from './commons/logging.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core/constants';
 
 @Module({
   imports: [UserModule, ArticleModule, CategoryModule, CommentModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },],
 })
 export class AppModule {}
